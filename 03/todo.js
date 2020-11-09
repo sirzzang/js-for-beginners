fe21cb45c1d965949f1ad5a3681b6e07ec4172a9
sirzzang <sirzzang@naver.com>
2020-11-09 15:54:52 +0900

    20201109 ~3.6 완료

--- 03/todo.js
+++ 03/todo.js
@@ -4,15 +4,29 @@ const toDoForm = document.querySelector('.js-toDoForm'),
 
 const TODOS_LS = 'toDos'; // 로컬 스토리지의 toDo 속성 키.
 
+const toDos = []; // 할 일 목록을 저장할 array.
+
+function saveToDos() {
+    localStorage.setItem(TODOS_LS, JSON.stringify(toDos)); // toDos 목록을 가져와서 로컬 스토리지에 저장한다.
+}
+
 function paintToDo(text) {
     const li = document.createElement('li'); // li 생성.
     const delBtn = document.createElement('button'); // 삭제를 위한 delete button 생성.
-    delBtn.innerText = '❌'; // 삭제 버튼에 이모지 삽입.
     const span = document.createElement('span'); // span 태그 생성.
+    const newId = toDos.length + 1; 
+    delBtn.innerText = '❌'; // 삭제 버튼에 이모지 삽입.    
     span.innerText = text; // submit 시 생성되어서 올 currentValue.
+    li.id = newId; // li에 id 속성 추가.
     li.appendChild(span); // li 아래에 span을 자식 요소로 넣는다.
     li.appendChild(delBtn); // li 아래에 delBtn을 자식 요소로 넣는다.
     toDoList.appendChild(li); // toDoList에 li를 자식 요소로 추가한다.
+    const toDoObj = {
+        text: text, 
+        id: newId        
+    };
+    toDos.push(toDoObj); // 할 일 목록 array에 할 일 object를 넣는다.
+    saveToDos(); // 로컬 스토리지에 저장한다.
 }
 
 function handleSubmit(event) {
@@ -23,9 +37,12 @@ function handleSubmit(event) {
 }
 
 function loadToDos() {
-    const toDos = localStorage.getItem(TODOS_LS); // 로컬 스토리지에서 toDo 속성값을 가져 온다.
-    if (toDos !== null) {
-        // toDo가 없는 경우의 작업만 필요하다.
+    const loadedToDos = localStorage.getItem(TODOS_LS); // 로컬 스토리지에서 toDo 속성값을 가져 온다.
+    if (loadedToDos !== null) {
+        const parsedToDos = JSON.parse(loadedToDos); // string 형태로 load된 할 일 목록을 JSON 형태로 파싱한다.
+        parsedToDos.forEach(function(toDo) {
+            paintToDo(toDo.text); // 각각의 toDo text에 대해 paintToDo 함수를 실행한다.
+        })  
     } 
 }
 
